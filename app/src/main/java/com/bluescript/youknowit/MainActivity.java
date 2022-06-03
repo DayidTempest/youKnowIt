048e309f2c6798ae3e76a4e783adeba0ee773f22
Jakub Sieradzki <49642112+jakub-sieradzki@users.noreply.github.com>
2022-06-03 23:44:56 +0200

    Foreground service basic implementation

--- app/src/main/java/com/bluescript/youknowit/MainActivity.java
+++ app/src/main/java/com/bluescript/youknowit/MainActivity.java
@@ -2,18 +2,30 @@ package com.bluescript.youknowit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static Intent serviceIntent = null;
+    NotificationsService notificationsService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
 
         FloatingActionButton fab = findViewById(R.id.addingActivities);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +34,44 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(v.getContext(), CreateAndEditSetActivity.class));
             }
         });
+
+        createAndBindService();
     }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            NotificationsService.NotificationsServiceBinder binder = (NotificationsService.NotificationsServiceBinder) service;
+            notificationsService = binder.getService();
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+        }
+    };
+
+    private void createAndBindService() {
+        serviceIntent = new Intent(this, NotificationsService.class);
+        startService(serviceIntent);
+        bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.foreground_channel_name);
+            String description = getString(R.string.foreground_channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("FOREGROUND_SERVICE_CHANNEL", name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 
 }
