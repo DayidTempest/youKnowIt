acf47955dfe9559bc8c6fad844f739a2978f8647
Jakub Sieradzki <49642112+jakub-sieradzki@users.noreply.github.com>
2022-06-03 20:55:45 +0200

    Ability to add new questions

--- app/src/main/java/com/bluescript/youknowit/CreateAndEditSetActivity.java
+++ app/src/main/java/com/bluescript/youknowit/CreateAndEditSetActivity.java
@@ -3,7 +3,9 @@ package com.bluescript.youknowit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -15,9 +17,12 @@ public class CreateAndEditSetActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_and_edit_set);
 
         FloatingActionButton fab = findViewById(R.id.AddQuestionFAB);
+        final LayoutInflater inflater = LayoutInflater.from(this);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ViewGroup parent = findViewById(R.id.AddRepeatsLinear);
+                inflater.inflate(R.layout.single_question_and_answer_tile, parent);
             }
         });
     }
