58aefbad6e6b4034cc59572020da2c28c7125dbf
DayidTempest <dpayerd@gmail.com>
2022-06-04 00:29:55 +0200

    save

--- app/src/main/java/com/bluescript/youknowit/CreateAndEditSetActivity.java
+++ app/src/main/java/com/bluescript/youknowit/CreateAndEditSetActivity.java
@@ -1,18 +1,28 @@
 package com.bluescript.youknowit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.UUID;
 
 public class CreateAndEditSetActivity extends AppCompatActivity {
     private ViewGroup parent;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +31,33 @@ public class CreateAndEditSetActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_create_and_edit_set);
         parent = findViewById(R.id.AddRepeatsLinear);
+        this.context = context;
+        MaterialToolbar materialToolbar = findViewById(R.id.topAppBar);
+        materialToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
 
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int itemID = item.getItemId();
+                if(itemID == R.id.action_save_set){
+                    UUID uuid = UUID.randomUUID();
+                    LinearLayout scroll = findViewById(R.id.AddRepeatsLinear);
+                    int childCount = scroll.getChildCount();
+
+                }
+                return false;
+            }
+        });
         FloatingActionButton fab = findViewById(R.id.AddQuestionFAB);
         final LayoutInflater inflater = LayoutInflater.from(this);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 ViewGroup parent = findViewById(R.id.AddRepeatsLinear);
                 inflater.inflate(R.layout.single_question_and_answer_tile, parent);
+
             }
         });
-
     }
 
     public void deleteButton(View v){
