c38f2836da74193ba0288d84881006e403ed8dc7
Jakub Sieradzki <49642112+jakub-sieradzki@users.noreply.github.com>
2022-06-03 21:17:28 +0200

    Ability to remove single question

--- app/src/main/java/com/bluescript/youknowit/CreateAndEditSetActivity.java
+++ app/src/main/java/com/bluescript/youknowit/CreateAndEditSetActivity.java
@@ -6,15 +6,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CreateAndEditSetActivity extends AppCompatActivity {
+    private ViewGroup parent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_and_edit_set);
+        parent = findViewById(R.id.AddRepeatsLinear);
 
         FloatingActionButton fab = findViewById(R.id.AddQuestionFAB);
         final LayoutInflater inflater = LayoutInflater.from(this);
@@ -25,5 +28,14 @@ public class CreateAndEditSetActivity extends AppCompatActivity {
                 inflater.inflate(R.layout.single_question_and_answer_tile, parent);
             }
         });
+
+    }
+
+    public void deleteButton(View v){
+        if(parent.getChildCount() > 1){
+            View view = (View)v.getParent();
+            int indexOfChildOfParent = parent.indexOfChild(view);
+            parent.removeViewAt(indexOfChildOfParent);
+        }
     }
 }
